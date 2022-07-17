460e792b4b624aaeede6cafe1d863fdc08eaf434
neonil <neonilr1999@gmail.com>
2022-07-17 18:33:39 +0300

    update

---
+++ libraries/Magellan_SIM7020E-1.5.0/README.md
@@ -0,0 +1,90 @@
+![Library Version](https://img.shields.io/badge/Version-1.5.0-green)
+
+# AIS Library
+
+## AIS SIM7020E API
+AIS SIM7020E API Library, used on arduino, have been developed for 
+any platform. This library include API such as UDP, MQTT, MQTTs
+### Example for use the AIS SIM7020E API
+#### Call the AIS SIM7020E API library:
+```cpp
+#include "AIS_SIM7020E_API.h"
+AIS_SIM7020E_API nb;
+```
+#### Initial AIS SIM7020E API Library:
+**for UDP**
+```cpp
+nb.begin(serverIP,serverPort);    
+```
+**for MQTT**
+```cpp
+nb.begin();    
+nb.connectMQTT(serverIP,serverPort,clientID)
+nb.RegisMQCallback(callback);     
+```
+#### Send Data:
+**for UDP**
+```cpp
+nb.sendMsgSTR(serverIP,serverPort,payload);  // Send data in String 
+// or
+nb.sendMsgHEX(serverIP,serverPort,payload);  // Send data in HexString   
+```
+**for MQTT**
+```cpp
+nb.publish(topic,payload)  
+``` 
+ **Note** please see more in the example code  
+ 
+## AIS Magellan Library
+AIS Magellan Library, a software development kit used on arduino platform, have been developed for 
+Magellan IoT Platform.  
+
+### Example for use the Magellan SDK
+#### Call the Magellan library:
+```cpp
+#include "Magellan_SIM7020E.h"
+Magellan_SIM7020E magel;
+```
+#### Initial Magellan Library:
+```cpp
+magel.begin();           //init Magellan LIB
+```
+#### Payload Data: 
+Please use the payload in JSON format 
+
+**Example**\
+{"Temperature":25,"Humidity":90}
+
+```cpp
+payload="{\"Temperature\":"+Temperature+",\"Humidity\":"+Humidity+"}";
+```
+#### Report Data:
+The example code report payload data to Magellan IoT Platform.
+```cpp
+magel.report(payload);
+```
+#### Example Magellan payload format
+Please the location payload data as below format.\
+**Example**
+```cpp
+payload="{\"Location\":"Latitude,Longitude"}";
+```
+Show battery on dashboard\
+Battery is range in 0-100 %.\
+**Example**
+```cpp
+payload="{\"Battery\":100}"; 
+```
+Show Lamp status on dashbord\
+please use 0 or 1 to send status\
+**Example**
+```cpp
+payload="{\"Lamp\":0}";
+payload="{\"Lamp\":1}";
+```
+**Note** please see more in the example code 
+## Quick Started
+  1. Connect `DEVIO NB-DEVKIT I` to your computer.
+  2. Open the Magellan IoT platform and see the data on your account.
+
+**Note** In this case, the device has already preload code then you just plug and play the development kit. If you have any questions, please see more details at https://www.facebook.com/AISDEVIO
---
+++ libraries/Magellan_SIM7020E-1.5.0/src/AT_SIM7020E.h
@@ -0,0 +1,149 @@
+/*
+Copyright (c) 2020, Advanced Wireless Network
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+AT Command Dictionary for SIMCOM SIM7020E version 1.4.2
+support SIMCOM SIM7020E
+NB-IoT with AT command
+
+Author: Device Innovation team  
+Create Date: 2 January 2020. 
+Modified: 19 May 2021.
+*/
+
+#include <Arduino.h>
+#include <Stream.h>
+
+struct pingRESP{
+	bool status;
+	String addr;
+	String ttl;
+	String rtt;
+};
+
+struct radio{
+	String pci="";
+	String rsrp="";
+	String rsrq="";
+	String snr="";
+};
+
+struct dateTime{
+	String date="";
+	String time="";
+};
+
+typedef void (*MQTTClientCallback)(String &topic, String &payload, String &QoS, String &retained);
+typedef void (*reponseCallback)(String &datax);
+
+class AT_SIM7020E{
+public:
+	AT_SIM7020E();
+	bool debug;	
+	//--------- Parameter config ---------------
+	const unsigned int msgLenMul=2;
+	const unsigned int mqttCmdTimeout=1200;
+	const unsigned int mqttBuffSize=1024;
+	//=========Initialization Module=======
+	void setupModule(String port="",String address="");
+	void check_module_ready();	
+	void reboot_module();
+	pingRESP pingIP(String IP);
+	bool closeUDPSocket();
+	bool NBstatus();
+	bool attachNetwork();
+	void powerSavingMode(unsigned int psm);
+	void syncLocalTime();
+
+	//==========Get Parameter Value=========
+	String getFirmwareVersion();
+	String getIMEI();
+	String getICCID();
+	String getIMSI();
+	String getDeviceIP();
+	String getSignal();    
+	String getAPN();
+	String getNetworkStatus();
+	radio getRadioStat();
+	bool checkPSMmode();
+	bool MQTTstatus();
+	dateTime getClock(unsigned int timezone);
+	//==========Data send/rec.===============
+	void waitResponse(String &retdata,String server);
+	void _Serial_print(String address,String port,unsigned int len);
+	void _Serial_print(String input);
+	void _Serial_print(unsigned int data);
+	void _Serial_print(char*);
+	void _Serial_println();
+	//===============Utility=================
+	void _serial_flush();	
+	//================MQTT===================
+	void disconnectMQTT();
+	bool newMQTT(String server, String port);
+	bool sendMQTTconnectionPacket(String clientID,String username,String password,int keepalive, int version,int cleansession, int willflag, String willOption);
+	void publish(String topic, String payload, unsigned int qos, unsigned int retained, unsigned int dup);
+	bool subscribe(String topic, unsigned int qos);
+	void unsubscribe(String topic);
+	unsigned int MQTTresponse();
+	String retTopic;
+  	String retPayload;
+  	String retQoS;
+  	String retRetained;
+  	int setCallback(MQTTClientCallback callbackFunc);
+	bool newMQTTs(String server, String port);
+  	bool setCertificate(byte cerType,int cerLength,byte isEnd,String CA);
+  	bool checkCertificate(int r_len,int c_len,int p_len);
+  	//============ callback ==================
+	reponseCallback callback_p;
+	MQTTClientCallback MQcallback_p;
+    
+private:
+	//==============Buffer====================
+	String data_input;
+	String data_buffer;
+	//==============Flag======================
+	bool hw_connected=false;
+	bool end=false;
+	//==============Parameter=================
+	unsigned int previous_check=0;
+	//============Counter value===============
+	byte k=0;
+	//==============Function==================
+	void echoOff();
+	bool setPhoneFunction();
+	void connectNetwork();
+	bool createUDPSocket(String address,String port);
+	void manageResponse(String &retdata,String server);
+	bool enterPIN();
+	void printHEX(char *str);
+	void blankChk(String& val);
+	void setAPN();
+
+protected:
+	Stream *_Serial;	
+};
